09921ba6c6dd4d81b6157f5b1c07bec1d1c54db0
Baxytra <root@laptop-baxytra.local>
2019-10-20 13:56:27 +0200

    add{temp-admin.php}: Première version du script

---
+++ temp-admin.php
@@ -0,0 +1,62 @@
+<?php
+
+function randomString($longueur = 10)
+{
+ return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($longueur/strlen($x)) )),1,$longueur);
+}
+
+function writeAccess($header, $footer, $randomURL)
+{
+	$rule = "\nRedirect /$randomURL /wp-admin";
+	$htaccess = file_get_contents('./.htaccess');
+	$htaccess = str_replace($header, '', $htaccess);
+	$htaccess = str_replace($footer, '', $htaccess);
+	$htaccess = preg_replace("/\nRedirect.*/", '', $htaccess);
+	$htaccess .= $header.$rule.$footer;
+	file_put_contents('./.htaccess', $htaccess);
+}
+
+function sendTelegramMessage($chatID, $messaggio, $token) {
+
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = $url . "&text=" . urlencode($messaggio);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+function checkSecret($secret, $yourSecret)
+{
+	if ($secret === $yourSecret) { return True; }
+	else { return False; }
+}
+
+
+// To define for telegram support
+$token = "";
+$chatid = "";
+
+// To define for this script
+$host = "www.baxytra.eu";
+$yourSecret = "abc";
+	
+if (!checkSecret($_GET["s"], $yourSecret)) { 
+	echo "Wrong secret";
+	exit();
+}
+
+
+$randomURL = "ta-".randomString(10);
+$header = "\n# BEGIN TEMP-ADMIN";
+$footer = "\n#END TEMP-ADMIN";
+
+writeAccess($header, $footer, $randomURL);
+sendTelegramMessage($chatid, "Host : $host\nRandom URI access is : https://$host/$randomURL", $token);
+echo "URI generated, look at your messages";
+?>
